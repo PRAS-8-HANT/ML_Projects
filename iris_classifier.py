0c7e18e32cea66638d3279aec7d28bcd5bf87f57
PRAS-8-HANT <161306779+PRAS-8-HANT@users.noreply.github.com>
2024-05-25 12:44:18 +0530

    Update iris_classifier.py

--- iris_classifier.py
+++ iris_classifier.py
@@ -15,7 +15,10 @@ sepal_width = st.number_input(label="Enter Sepal Width",placeholder="Enter Sepal
 petal_length = st.number_input(label="Enter Petal Length",placeholder="Enter Petal Length",value=None,min_value=0.69,max_value=11.1,step=0.1)
 petal_width = st.number_input(label="Enter Petal Width",placeholder="Enter Petal Width",value=None,min_value=0.69,max_value=11.1,step=0.1)
 
-x = df[['sepal_length','sepal_width','petal_length','petal_width']]
+data = [{'sepal_length': sepal_length, 'sepal_width': sepal_width,'petal_length': petal_length,'petal_width':petal_width}]
+col = ['sepal_length','sepal_width','petal_length','petal_width']
+
+df = pd.DataFrame(data,columns=col)
 
 pred = st.button("Predict",use_container_width=True)
 
@@ -23,7 +26,7 @@ if pred:
     if any([sepal_length is None, sepal_width is None, petal_length is None, petal_width  is None]):
         st.error("Please, Select all Inputs before Pressing Predict Button.",icon="📝")
     else:
-        prediction = int(model.predict(x))
+        prediction = int(model.predict(df))
         if prediction == "":
             st.error("Please select Valid Inputs.", icon="⚠️")
         else:
