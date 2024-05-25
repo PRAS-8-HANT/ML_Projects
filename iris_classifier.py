1bade9b7fcc92f396a5052045f0564eb33da905f
PRAS-8-HANT <161306779+PRAS-8-HANT@users.noreply.github.com>
2024-05-25 17:30:48 +0530

    Update iris_classifier.py

--- iris_classifier.py
+++ iris_classifier.py
@@ -28,8 +28,7 @@ if pred:
     if any([sepal_length is None, sepal_width is None, petal_length is None, petal_width  is None]):
         st.error("Please, Select all Inputs before Pressing Predict Button.",icon="📝")
     else:
-        prediction = model.predict(df)
-        prediction = listToString(prediction)
+        prediction = listToString(model.predict(df))        
         if prediction == "":
             st.error("Please select Valid Inputs.", icon="⚠️")
         else:
